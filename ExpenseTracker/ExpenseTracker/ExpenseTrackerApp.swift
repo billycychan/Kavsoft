d69c7baa6a0acf1b47cb585aa66060c2c50af62d
Billy Chan <billycychan.work@gmail.com>
2023-12-22 08:34:59 -0500

    feat(expense-tracker): initiate project

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by CHI YU CHAN on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
