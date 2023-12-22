2e203cc34baef7f515d4db446526cc0a407ecaab
Billy Chan <billycychan.work@gmail.com>
2023-12-22 08:59:25 -0500

    feat(expense-tracker): finish the intro screen

--- ExpenseTracker/ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ExpenseTracker/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// Visibility Status
+    @AppStorage("isFirstTime") private var isFirstTime: Bool = true
+    
+    @State private var activeTab: Tab = .recents
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $activeTab) {
+            Text(Tab.recents.rawValue)
+                .tag(Tab.recents)
+                .tabItem { Tab.recents.tabContent }
+            Text(Tab.recents.rawValue)
+                .tag(Tab.search)
+                .tabItem { Tab.search.tabContent }
+            Text(Tab.charts.rawValue)
+                .tag(Tab.recents)
+            
+                .tabItem { Tab.recents.tabContent }
+            Text(Tab.settings.rawValue)
+                .tag(Tab.settings)
+                .tabItem { Tab.settings.tabContent }
         }
-        .padding()
+        .tint(appTint)
+        .sheet(isPresented: $isFirstTime) {
+            IntroScreen()
+                .interactiveDismissDisabled()
+        }
     }
 }
 
---
+++ ExpenseTracker/ExpenseTracker/Helpers/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  ExpenseTracker
+//
+//  Created by CHI YU CHAN on 22/12/2023.
+//
+
+import SwiftUI
+
+let appTint: Color = .blue
