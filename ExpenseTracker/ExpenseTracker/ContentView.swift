970bdb44ca047cbe870c8b6dd3d91eaec8e9a925
Billy Chan <billycychan.work@gmail.com>
2023-12-23 20:53:58 -0500

    feat(expense-tracker): finish the app lock view

--- ExpenseTracker/ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ExpenseTracker/ContentView.swift
@@ -10,29 +10,34 @@ import SwiftUI
 struct ContentView: View {
     /// Visibility Status
     @AppStorage("isFirstTime") private var isFirstTime: Bool = true
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
     
     @State private var activeTab: Tab = .recents
     
     var body: some View {
-        TabView(selection: $activeTab) {
-            Recents()
-                .tag(Tab.recents)
-                .tabItem { Tab.recents.tabContent }
-            Search()
-                .tag(Tab.search)
-                .tabItem { Tab.search.tabContent }
-            Graphs()
-                .tag(Tab.charts)
-                .tabItem { Tab.charts.tabContent }
-            Settings()
-                .tag(Tab.settings)
-                .tabItem { Tab.settings.tabContent }
+        LockView(lockType: .biometric, lockPin: "", isEnabled: isAppLockEnabled, lockWhenAppGoesBackground: lockWhenAppGoesBackground) {
+            TabView(selection: $activeTab) {
+                Recents()
+                    .tag(Tab.recents)
+                    .tabItem { Tab.recents.tabContent }
+                Search()
+                    .tag(Tab.search)
+                    .tabItem { Tab.search.tabContent }
+                Graphs()
+                    .tag(Tab.charts)
+                    .tabItem { Tab.charts.tabContent }
+                Settings()
+                    .tag(Tab.settings)
+                    .tabItem { Tab.settings.tabContent }
+            }
+            .tint(appTint)
+            .sheet(isPresented: $isFirstTime) {
+                IntroScreen()
+                    .interactiveDismissDisabled()
+            }
         }
-        .tint(appTint)
-        .sheet(isPresented: $isFirstTime) {
-            IntroScreen()
-                .interactiveDismissDisabled()
-        }
+        
     }
 }
 
--- WWDC23/LockSwiftUIView/LockSwiftUIView/View/LockView.swift
+++ WWDC23/LockSwiftUIView/LockSwiftUIView/View/LockView.swift
@@ -95,6 +95,10 @@ struct LockView<Content: View>: View {
                 isUnlocked = false
                 pin = ""
             }
+            
+            if newValue == .active && !isUnlocked && isEnabled {
+                unlockView()
+            }
         }
     }
     
