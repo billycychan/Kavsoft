3515ac6c13387f217efc7e0bd5fe3d0ccf6f122f
Billy Chan <billycychan.work@gmail.com>
2023-12-25 16:12:23 -0500

    feat(expense-tracker): implement the widget

---
+++ ExpenseTracker/StatsCardView/StatsCardViewBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StatsCardViewBundle.swift
+//  StatsCardView
+//
+//  Created by CHI YU CHAN on 25/12/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StatsCardViewBundle: WidgetBundle {
+    var body: some Widget {
+        StatsCardView()
+    }
+}
